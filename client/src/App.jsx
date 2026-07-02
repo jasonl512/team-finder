31850b68a81a1874158dec6442bd39e8f0009777
Jason Lu <2673414599lu@gmail.com>
2026-07-02 00:27:57 -0400

    Add player profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,4 +4,19 @@ import Home from './pages/Home';
 import TeamFinder from './pages/TeamFinder';
 import Messages from './pages/Messages';
 import GameDiscovery from './pages/GameDiscovery';
-export default function App(){return <><Navbar/><Routes><Route path='/' element={<Home/>}/><Route path='/find-team' element={<TeamFinder/>}/><Route path='/messages' element={<Messages/>}/><Route path='/games' element={<GameDiscovery/>}/></Routes></>}
+import Profile from './pages/Profile';
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/find-team" element={<TeamFinder />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/games" element={<GameDiscovery />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </>
+  );
+}
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,2 +1,18 @@
 import { NavLink } from 'react-router-dom';
-export default function Navbar(){return <nav className='navbar'><div className='container nav-inner'><NavLink to='/' className='brand'><span className='logo'>🎮</span><span>TEAM FINDER</span></NavLink><div className='nav-links'><NavLink to='/'>Home</NavLink><NavLink to='/find-team'>Find Team</NavLink><NavLink to='/messages'>Messages</NavLink><NavLink to='/games'>Game Discovery</NavLink></div></div></nav>}
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="container nav-inner">
+        <NavLink to="/" className="brand"><span className="logo">🎮</span><span>TEAM FINDER</span></NavLink>
+        <div className="nav-links">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/find-team">Find Team</NavLink>
+          <NavLink to="/messages">Messages</NavLink>
+          <NavLink to="/games">Game Discovery</NavLink>
+          <NavLink to="/profile">Profile</NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+}
