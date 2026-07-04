6ebbabd7443d13e85bc86ab2ab97f5fe1f3879ea
Joel Lee <109010098+jojule21@users.noreply.github.com>
2026-07-04 13:39:43 -0400

    Add files via upload

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,25 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import ProtectedRoute from './components/ProtectedRoute';
 import Home from './pages/Home';
+import Login from './pages/Login';
 import TeamFinder from './pages/TeamFinder';
 import Messages from './pages/Messages';
 import GameDiscovery from './pages/GameDiscovery';
-import Profile from './pages/Profile';
 
 export default function App() {
   return (
     <>
       <Navbar />
       <Routes>
+        {/* Public pages */}
         <Route path="/" element={<Home />} />
-        <Route path="/find-team" element={<TeamFinder />} />
-        <Route path="/messages" element={<Messages />} />
         <Route path="/games" element={<GameDiscovery />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+
+        {/* Pages that require login */}
+        <Route path="/find-team" element={<ProtectedRoute><TeamFinder /></ProtectedRoute>} />
+        <Route path="/messages" element={<ProtectedRoute><Messages /></ProtectedRoute>} />
       </Routes>
     </>
   );
